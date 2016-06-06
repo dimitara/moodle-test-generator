232c943d59d1626190eafb86c68168990532f61a
Dimitar Atanassov <dimitara@gmail.com>
2016-06-06 20:14:13 +0300

    artificial link to download file

--- public/js/app.js
+++ public/js/app.js
@@ -215,34 +215,15 @@ var sendChecked = function(){
     }
     
     api('data.php/pdfgen', 'POST', payload, function(data){
-        filename = 'test.pdf';
+        var url = window.location.href;
+        url = url.substring(0,url.lastIndexOf('/') + 1);
+        url = url + data.quiz_file_path;
+        console.log(url);
+        var a = document.createElement('a');
+        a.setAttribute('href', url);
+        a.setAttribute('target', '_blank');
 
-        var blob = new Blob([data], { type: 'application/pdf' });
-        if (typeof window.navigator.msSaveBlob !== 'undefined') {
-            // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
-            window.navigator.msSaveBlob(blob, filename);
-        } else {
-            var URL = window.URL || window.webkitURL;
-            var downloadUrl = URL.createObjectURL(blob);
-
-            if (filename) {
-                // use HTML5 a[download] attribute to specify filename
-                var a = document.createElement("a");
-                // safari doesn't support this yet
-                if (typeof a.download === 'undefined') {
-                    window.location = downloadUrl;
-                } else {
-                    a.href = downloadUrl;
-                    a.download = filename;
-                    document.body.appendChild(a);
-                    a.click();
-                }
-            } else {
-                window.location = downloadUrl;
-            }
-
-            setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
-        }
+        а.click();
     });
 };
 
